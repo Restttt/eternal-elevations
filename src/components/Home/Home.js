7a36c8552d4fd6fad7ea05b05be66298aa6a29d6
tanner <tanner@gosolo.io>
2019-12-03 16:27:31 -0700

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const Home = (props) => {
+  return (
+    <div>
+      <Typography variant="h4">
+        Hello
+      </Typography>
+    </div>
+  )
+};
+
+export default Home;
